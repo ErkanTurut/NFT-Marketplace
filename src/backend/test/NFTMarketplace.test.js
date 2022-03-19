a7c979cab77c47329d81e1044a65fcebfeecc1f1
SirKamso <erkamturut@gmail.com>
2022-03-20 00:09:56 +0100

    purchase item contract done, test not working

--- src/backend/test/NFTMarketplace.test.js
+++ src/backend/test/NFTMarketplace.test.js
@@ -1,5 +1,8 @@
 const { expect } = require("chai");
 
+const toWei = (num) => ethers.utils.parseEther(num.toString());
+const fromWei = (num) => thers.utils.formatEther(num);
+
 describe("NFTMarketplace", function () {
   let deployer, addr1, addr2, nft, marletplace;
   let feePercent = 1;
@@ -28,59 +31,105 @@ describe("NFTMarketplace", function () {
     });
   });
 
-  describe("Minting NFT's", function () {
-    it("should track each minted NFT", async function () {
-      //addr1 mint an nft
+  describe("Minting NFTs", function () {
+    it("Should track each minted NFT", async function () {
+      // addr1 mints an nft
       await nft.connect(addr1).mint(URI);
-      expect(await nft.tokenCount()).to.equal.apply(1);
+      expect(await nft.tokenCount()).to.equal(1);
       expect(await nft.balanceOf(addr1.address)).to.equal(1);
-      expect(await nft.tokenURI(1)).to.equal.apply(URI);
-
-      //addr2 mint an nft
+      expect(await nft.tokenURI(1)).to.equal(URI);
+      // addr2 mints an nft
       await nft.connect(addr2).mint(URI);
-      expect(await nft.tokenCount()).to.equal.apply(2);
+      expect(await nft.tokenCount()).to.equal(2);
       expect(await nft.balanceOf(addr2.address)).to.equal(1);
-      expect(await nft.tokenURI(2)).to.equal.apply(URI);
+      expect(await nft.tokenURI(2)).to.equal(URI);
     });
   });
 
-  describe("making marketplace items", function () {
+  describe("Making marketplace items", function () {
+    let price = 1;
+    let result;
     beforeEach(async function () {
-      //addr 1 mints an nft
+      // addr1 mints an nft
       await nft.connect(addr1).mint(URI);
-      //addr1 pproves marketplace to spend nft
+      // addr1 approves marketplace to spend nft
       await nft.connect(addr1).setApprovalForAll(marketplace.address, true);
     });
-    it("should track newly created item, transfer NFT from seller to marketplace and emit Offered event", async function () {
-      //add1 offers their nft at a price of 1 ether
+
+    it("Should track newly created item, transfer NFT from seller to marketplace and emit Offered event", async function () {
+      // addr1 offers their nft at a price of 1 ether
       await expect(
-        marketplace.connect(addr1).makeItem(nft.address, 1, toWei(1))
+        marketplace.connect(addr1).makeItem(nft.address, 1, toWei(price))
       )
-        .to.emit(matketplace, "offered")
-        .withArgs(1, nft.address, 1, toWei(1), addr1.address);
-
-      //awner of NFT should now be the marketplace
+        .to.emit(marketplace, "Offered")
+        .withArgs(1, nft.address, 1, toWei(price), addr1.address);
+      // Owner of NFT should now be the marketplace
       expect(await nft.ownerOf(1)).to.equal(marketplace.address);
-      //item count should new equal 1
+      // Item count should now equal 1
       expect(await marketplace.itemCount()).to.equal(1);
-
-      //get item from items mapping then check fields to ensure they are correct
-
+      // Get item from items mapping then check fields to ensure they are correct
       const item = await marketplace.items(1);
-      expect(item.itemID).to.equal(1);
+      expect(item.itemId).to.equal(1);
       expect(item.nft).to.equal(nft.address);
       expect(item.tokenId).to.equal(1);
-      expect(item.price).to.equal(toWei(1));
+      expect(item.price).to.equal(toWei(price));
       expect(item.sold).to.equal(false);
     });
 
-    it("should fail if price is set to zero", async function () {
+    it("Should fail if price is set to zero", async function () {
       await expect(
-        marketplace
-          .connect(addr1)
-          .marketItem(nft.address, 1, 0)
-          .to.be.revertedtWith("Price must be greater than zero")
+        marketplace.connect(addr1).makeItem(nft.address, 1, 0)
+      ).to.be.revertedWith("Price must be greater than zero");
+    });
+  });
+  describe("Purchasing marketplace items", function () {
+    let price = 1;
+    beforeEach(async function () {
+      //addr1 mints an nft
+      await nft.connect(addr1).mint(URI);
+      //addr1 approves marketplace to spend nft
+      await nft.connect(addr1).setApprovalForAll(marketplace.address, true);
+      //addr1 makes their nft a marketplace item
+      await marketplace.connect(addr1).makeItem(nft.address, 1, toWei(2));
+    });
+    it("should uptade item as sold, pay seller transfer NFT to buyer, charge fees and emit a bought event", async function () {
+      const sellerInitalEthBal = await addr1.getBalance();
+      const feeAccountInitialEthBal = await deployer.getBalance();
+      //fetch item total price (market fees + item price)
+      let totalPriceInWei = await marketplace.getTotalPrice(1);
+      //addr 2 urchase item
+      await expect(
+        marketplace.connect(addr2).purchaseItem(1, { value: totalPriceInWei })
+      )
+        .to.emit(marketplace, "Bought")
+        .withArgs(
+          1,
+          nft.address,
+          1,
+          toWei(price),
+          addr1.address,
+          addr2.address
+        );
+
+      const sellerFinalEthBal = await addr1.getBalance();
+      const feeAccountFinalEthBal = await deployer.getBalance();
+
+      //seller should receive de payement for the price of the NFT
+      expect(+fromWei(sellerFinalEthBal)).to.equal(
+        +price + +fromWei(sellerInitalEthBal)
       );
+
+      //calculate the fee price
+      const fee = (feePercent / 100) * price;
+      expect(toWei(fee)).to.equal(totalPriceInWei - price);
+      //feeAccount should receive de fee of de buyer's payement
+      expect(+fromWei(feeAccountFinalEthBal)).to.equal(
+        +fee + +fromWei(feeAccountInitialEthBal)
+      );
+      //the buyer should own the NFT
+      expect(await nft.ownerOf(1)).to.equal.apply(addr2.address);
+      //Item should be marked as sold
+      expect((await marletplace.items(1)).sold).to.equal(true);
     });
   });
 });
